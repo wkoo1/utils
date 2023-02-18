d7784fd2be341416cb864a930cb7325f11f0d6f7
Wang <wkoo1@163.com>
2023-02-18 17:44:12 +0900

    used

--- pdf_merge.py
+++ pdf_merge.py
@@ -2,8 +2,8 @@ from pypdf import PdfWriter
 
 merger = PdfWriter()
 
-for pdf in ["1.pdf", "2.pdf", "3.pdf"]:
+for pdf in ["F:/Dropbox/application/Overleaf/wangkai-修論/00titlepage.pdf", "F:/Dropbox/application/Overleaf/wangkai-修論-概要/abstract.pdf", "F:/Dropbox/application/Overleaf/wangkai-修論/MasterThesis_WangKai.pdf"]:
     merger.append(pdf)
 
-merger.write("MasterThesis_WangKai.pdf")
+merger.write("F:/Dropbox/application/Overleaf/wangkai-修論/MasterThesis_WangKai_Complete.pdf")
 merger.close()

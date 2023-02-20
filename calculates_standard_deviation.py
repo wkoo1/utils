ba8b2baec7c37d6c3a5ec25a54d3a451f8223757
wkoo1 <wkoo1@163.com>
2023-02-20 12:36:39 +0900

    lab

--- calculates_standard_deviation.py
+++ calculates_standard_deviation.py
@@ -3,5 +3,5 @@ def std(nums):
     avg = sum(nums) / n
     return avg,(sum(map(lambda e: (e - avg) * (e - avg), nums)) / n) ** 0.5
 
-nums = [0.822,0.8184,0.8526,0.814,0.8604,0.8478,0.8404,0.859,0.8574]
+nums = [0.8545,0.8553,0.8950,0.8822,0.9035,0.8968,0.8999,0.9137,0.9045]
 print(std(nums))
--- pdf_merge.py
+++ pdf_merge.py
@@ -2,7 +2,7 @@ from pypdf import PdfWriter
 
 merger = PdfWriter()
 
-for pdf in ["D:/oukai/Dropbox/application/Overleaf/wangkai-修論/00titlepage.pdf", "D:/oukai/Dropbox/application/Overleaf/wangkai-修論/0abstract.pdf", "D:/oukai/Dropbox/application/Overleaf/wangkai-修論/MasterThesis_WangKai.pdf"]:
+for pdf in ["D:/oukai/Dropbox/application/Overleaf/wangkai-修論/00titlepage.pdf", "D:/oukai/Dropbox/application/Overleaf/wangkai-修論-概要/abstract.pdf", "D:/oukai/Dropbox/application/Overleaf/wangkai-修論/MasterThesis_WangKai.pdf"]:
     merger.append(pdf)
 
 merger.write("D:/oukai/Dropbox/application/Overleaf/wangkai-修論/MasterThesis_WangKai_Complete.pdf")
